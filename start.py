67ff6251a7edd3b8bfd938ecffc2704ed12a83a6
ilmacoder <82860778+ilmacoder@users.noreply.github.com>
2021-04-20 12:57:32 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://GitHub.com/ULTRA-OP/ULTRA-X && cd ULTRA-X && python3 -m ULTRA")
+system ("git clone https://github.com/ilmacoder/Vader && cd Vader && python3 -m userbot")
 # try af
 print("OH SYSTEM IS CRASHED")
-system("python3 -m ULTRA")
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
