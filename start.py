890ed91ab6a8b9c6f863956d1b59070239620a84
ilmacoder <82860778+ilmacoder@users.noreply.github.com>
2021-04-20 14:57:14 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/ilmacoder/Vader && cd Vader && python3 -m userbot")
+system ("git clone https://github.com/thevaders/vader && cd vader && python3 -m userbot")
 # try af
 print("OH SYSTEM IS CRASHED")
